b149926b214deaf821cb9ef9bff7f7c9d7cfb2d4
Melchior Natthan <85684770+melchiornatthan@users.noreply.github.com>
2024-02-15 10:56:13 +0700

    Refactor admin and user dashboard layouts

--- src/pages/admin/adminDashboard.js
+++ src/pages/admin/adminDashboard.js
@@ -5,26 +5,16 @@ import ProviderCount from "../../components/providerCount";
 import RequestCount from "../../components/requestCount";
 
 function AdminDashboard() {
-
   return (
     <Container>
-    <Row className="mx-auto centered-row">
-      <Col md className="mx-2">
-        <RequestCount/>
-      </Col>
-      <Col
-        md
-        style={{
-          marginTop: "1vh",
-          marginBottom: "1vh",
-          backgroundColor: "#FFFFFF",
-          borderRadius: "1vh",
-          paddingBottom: "3vh",
-        }}
-      >
-        <ProviderCount/>
-      </Col>
-    </Row>
+      <Row>
+        <Col>
+          <RequestCount />
+        </Col>
+        <Col>
+          <ProviderCount />
+        </Col>
+      </Row>
     </Container>
   );
 }
--- src/pages/user/Dashboard.js
+++ src/pages/user/Dashboard.js
@@ -1,57 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import React from "react";
 import "typeface-inter";
-import ResponsiveDoughnutChart from "../../components/doughnutChart";
-import { Container, Row, Col, Card,Image} from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
-import { FaConciergeBell } from "react-icons/fa";
-import { MdBuildCircle } from "react-icons/md";
-import { FaTruckMoving } from "react-icons/fa";
-import { IoMdRemoveCircle } from "react-icons/io";
-import logo from "../../assets/logo-nisb.png";
-import { getRequestCount } from "../../service/getRequestCount";
-import { getProviderCount } from "../../service/getProviderCount";
+import { Container, Row, Col } from "react-bootstrap";
 import ProviderCount from "../../components/providerCount";
 import RequestCount from "../../components/requestCount";
 
 function Dashboard() {
-  ChartJS.register(ArcElement, Tooltip, Legend);
-  const [providerCount, setProviderCount] = useState([]);
-  const [reqCount, setReqCount] = useState([]);
-  const [date, setDate] = useState(new Date());
-  const navigate = useNavigate();
-  const data = [
-    { name: "Primacom", value: parseInt(providerCount.primacom) },
-    { name: "Tangara", value: parseInt(providerCount.tangara) },
-    { name: "IFORTE", value: parseInt(providerCount.iforte) },
-    { name: "Indonet", value: parseInt(providerCount.indonet) },
-  ];
-
-  useEffect(() => {
-    getRequestCount(setReqCount);
-    getProviderCount(setProviderCount, setDate);
-  }, []);
-
-  
-
   return (
-    <Row className="mx-auto centered-row">
-      <Col md className="mx-3">
-       <RequestCount/>
+    <Container>
+    <Row>
+      <Col>
+        <RequestCount />
       </Col>
-      <Col
-        md
-        style={{
-          marginTop: "2vh",
-          marginBottom: "1vh",
-          backgroundColor: "#FFFFFF",
-          borderRadius: "1vh",
-          paddingBottom: "3vh",
-        }}
-      >
-       <ProviderCount/>
+      <Col>
+        <ProviderCount />
       </Col>
     </Row>
+  </Container>
   );
 }
 
